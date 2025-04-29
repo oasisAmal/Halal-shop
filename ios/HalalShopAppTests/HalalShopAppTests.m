5f3a3b6d51006943b26ba0cbb8bbf0c99f1081d4
Deepa <143608314+AshikaMRFathima@users.noreply.github.com>
2025-04-29 18:07:23 +0400

    PACKAGE NAME CHANGES

--- ios/HalalShopAppTests/HalalShopAppTests.m
+++ ios/HalalShopAppTests/HalalShopAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ZabehatyShopAppTests : XCTestCase
+@interface HalalShopAppTests : XCTestCase
 
 @end
 
-@implementation ZabehatyShopAppTests
+@implementation HalalShopAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
